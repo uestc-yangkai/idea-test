0dab7d1d99a1ec0bfefd8410340483c64fb704b2
yangkai <2278976316@qq.com>
2018-05-11 16:59:35 +0800

    第一次提交

---
+++ src/main/java/com/demo01/Hello.java
@@ -0,0 +1,10 @@
+package com.demo01;
+
+/**
+ * Created by kaizige on 2018/5/11.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
